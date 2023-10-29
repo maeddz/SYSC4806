f346e03f46fa7d74805d44dca18d09be2749d7dc
m.davoodzade <m.davoodzade@arnikup.com>
2023-10-29 02:18:02 -0400

    add buddyinfo

--- target/classes/static/file.js
+++ target/classes/static/file.js
@@ -1,27 +1,45 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
-
-    $(document).on("submit", "#createaddressbook", function(event){
+    $(document).on("submit", "#createaddressbook", function (event) {
         $.ajax({
             type: $("#createaddressbook").attr("method"),
             url: '/addressBook'
-        }).done(function(response) {
-            let body = response.substring(response.indexOf("<body>")+6,response.indexOf("</body>"));
+        }).done(function (response) {
+            let body = response.substring(response.indexOf("<body>") + 6, response.indexOf("</body>"));
             $("body").html(body);
         });
         alert("created!");
         event.preventDefault();
     });
 
-    $(document).on("submit", "#viewaddressbook", function(event){
+    $(document).on("submit", "#viewaddressbook", function (event) {
         $.ajax({
             type: $('#viewaddressbook').attr("method"),
             url: '/addressBook',
             data: $('#viewaddressbook').serialize(),
-        }).done(function(response) {
-            let body = response.substring(response.indexOf("<body>")+6,response.indexOf("</body>"));
+        }).done(function (response) {
+            let body = response.substring(response.indexOf("<body>") + 6, response.indexOf("</body>"));
             $("body").html(body);
         });
         event.preventDefault();
     });
 });
+
+function createBuddy() {
+    let data = {
+        "name": $("#name").val(),
+        "number": $("#number").val(),
+    };
+    addrId = $("#addrId").val();
+    $.ajax({
+        type: 'post',
+        url: '/addressBook/' + addrId,
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    }).done(function (response) {
+        let body = response.substring(response.indexOf("<body>") + 6, response.indexOf("</body>"));
+        $("body").html(body);
+    });
+    alert("buddy added!");
+    location.reload();
+}
